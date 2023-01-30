07f40b8fdf6bd04d4a1ce85bee4bb8f963086968
Céline Gourc <celine.gourc1@laposte.net>
2023-01-30 18:48:02 +0100

    Création de la fenêtre modale

--- FrontEnd/connexion.js
+++ FrontEnd/connexion.js
@@ -1,41 +1,40 @@
 
-let email=document.querySelector("#email");
-let password=document.querySelector("#password")
+let email = document.querySelector("#email");
+let password = document.querySelector("#password")
+
+document.querySelector("#loginButton").style.fontWeight ="bold"
 
 document.querySelector("form").addEventListener("submit",function(event){
   event.preventDefault() 
-  
-let emailValue=(email).value;
-let passwordValue = (password).value;
-
- fetch("http://localhost:5678/api/users/login", {
-    
+  let emailValue=email.value;
+  let passwordValue = password.value;
+  fetch("http://localhost:5678/api/users/login", {
       method: 'POST', 
       headers: {
-        
         'Content-Type': 'application/json;charset=utf-8',
       },
       body: JSON.stringify({ 
         "email": emailValue,
-        "password": passwordValue})
+        "password": passwordValue
+      })
     })
-      .then(response => response.json())
+      .then(response => response.json()
+      )
       .then(data => {
-        if(data.token==undefined){
-        alert("Adresse email ou mot de passe incorrect")
-        }else{
-          //let token = data.token
-          //console.log(token)
-          window.location.replace('index.html')
-          let newH2= document.querySelector("h2")
-          newH2.innerText= "Je viens du Log In"
+        if(data.token===undefined){
+          alert("Adresse email ou mot de passe incorrect")
+        } else {
+            function fillingStockage(){
+            localStorage.setItem("token", data.token);
+          }
+          fillingStockage()
+          window.location.replace("index.html")
         }
       })
 
       .catch((error) => {
-        console.log('Erreur : ' + error.message);
-
+        console.log("Erreur : " + error.message);
       });
-    })
+})
 
   
--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,9 +1,5 @@
+document.querySelector("#modalWindow").style.display ="none";
 
-
-window.addEventListener('location.replace', ()=>{
-let newH2= document.querySelector("h2")
-newH2.innerText= "Je viens du Log In"})
-  
 function displayWork(work){
   const workFigure = document.createElement("figure")
   const workImg = document.createElement("img")
@@ -16,75 +12,98 @@ function displayWork(work){
   workFigure.appendChild(workFigcaption)
 }
 
-function ajoutStyleHover(element){
-  element.addEventListener("mouseenter",()=>{
-    element.style.background ="#1D6154"
-    element.style.color ="white"
-    element.style.cursor="pointer"})
-}
-function removeStyleHover(element){
-  element.addEventListener("mouseleave",()=>{
-  element.style.background ="white"
-  element.style.color ="#1D6154"
-  element.style.cursor="pointer"})
-}
-
-
 const workButtonContainer = document.createElement("ul")
+workButtonContainer.classList.add("categoryNav")
 workButtonContainer.classList.add('button_container')
 document.querySelector(".gallery").before(workButtonContainer)
 
- 
 
 fetch("http://localhost:5678/api/works")
-    .then(function(response) {
-      if (response.ok) {
-        return response.json();
-      }
-    })
+  .then(function(response) {
+    if (response.ok) {
+      return response.json();
+    }
+  })
 
-    .then(function(data){
+  .then(function(data){
 
-      for(let work of data){ 
-        displayWork(work)
-      }
+    const categorySet = new Set();
+    for(let work of data){ 
+      displayWork(work)
+      displayWorkEdition(work)
+      categorySet.add("Tous")
+      categorySet.add(work.category.name)
+    }
 
-
-      const monSet = new Set();
-
-      for(let work of data){
-        monSet.add("Tous")
-        monSet.add(work.category.name)
-       }
-
-      for(let element of monSet){
-        let workButton = document.createElement("li")
-        workButton.classList.add('buttons')
-          if (typeof element==="string"){
-          workButton.innerText=element
-          workButtonContainer.appendChild(workButton)}
-        }
+    for(let element of categorySet){
+      let workButton = document.createElement("li")
+      workButton.classList.add('buttons')
+      workButton.innerText=element
+      workButtonContainer.appendChild(workButton)
+    }
         
-        let buttons = document.querySelectorAll('.buttons')
-        for(let button of buttons){
-          
-          button.addEventListener("click",() => {
-            document.querySelector(".gallery").innerHTML=" ";
-            for (let work of data){
-              if (button.innerText === work.category.name){
+    let categoryButtons = document.querySelectorAll('.buttons')
+    for(let button of categoryButtons){
+        button.addEventListener("click",() => {
+          document.querySelector(".gallery").innerHTML=" ";  
+          for (let work of data){
+            if (button.innerText === work.category.name){
+              displayWork(work)
+              } else if (button.innerText === "Tous") {
                 displayWork(work)
-                }else if (button.innerText === "Tous"){
-                  displayWork(work)
-                  }
-
               }
-            })
-          ajoutStyleHover(button)
-          removeStyleHover(button)
-          }
+            }
+        })
+    }
+  })
+  .catch(function(err) {
+    console.log('Une erreur est survenue',err)
+  })
 
-      })
-     
-        .catch(function(err) {
-        console.log('Une erreur est survenue',err)
-      })
+
+let userToken = localStorage.getItem("token")
+
+if(userToken){
+  document.querySelector("#loginButton").remove()
+  document.querySelector(".categoryNav").remove()
+  function deconnect(){
+    localStorage.clear("token")
+  }
+  document.querySelector("#logoutButton").addEventListener("click", ()=>{
+    deconnect()
+    window.location.replace('index.html')
+  })
+
+  document.querySelector("#modalLink").addEventListener("click", ()=>{
+    document.querySelector("#modalWindow").style.display ="flex";
+  })
+  document.querySelector(".fa-xmark").addEventListener("click", ()=>{
+    document.querySelector("#modalWindow").style.display="none";
+  })
+} else {
+    let modifyTag= document.querySelectorAll(".modification")
+    for (let tag of modifyTag){
+      tag.remove()
+    }
+    document.querySelector("#logoutButton").remove()
+    document.querySelector(".editionMode").remove() 
+}
+
+function displayWorkEdition(work){
+const figureEdition = document.createElement("figure")
+const deleteIcon  = document.createElement("div")
+deleteIcon.innerHTML= `<i class="fa-solid fa-trash-can"></i>`
+const imgEdition = document.createElement("img")
+imgEdition.setAttribute("crossorigin", "anonymous")
+imgEdition.src = work.imageUrl
+const figcaptionEdition = document.createElement("figcaption")
+figcaptionEdition.innerHTML = "éditer"
+document.querySelector(".galleryEdition").appendChild(figureEdition)
+figureEdition.appendChild(deleteIcon)
+figureEdition.appendChild(imgEdition)
+figureEdition.appendChild(figcaptionEdition)}
+
+
+
+
+
