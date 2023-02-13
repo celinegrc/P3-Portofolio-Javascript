241923fb9938ef450c50398f4bb1e96bb41ba603
Céline Gourc <celine.gourc1@laposte.net>
2023-02-13 12:29:29 +0100

    modification de l'emplacement des messages d'erreurs de connexion

--- FrontEnd/connexion.js
+++ FrontEnd/connexion.js
@@ -1,14 +1,14 @@
 
-/* Style de l'item "login" du menu*/ 
+// Style de l'item "login" du menu
 document.querySelector("#loginButton").style.fontWeight ="bold"
-
 
 document.querySelector("form").addEventListener("submit",function(event){
   event.preventDefault() 
 
+//Récupération des valeurs entrées dans le formulaire
   let email = document.querySelector("#email");
-  let password = document.querySelector("#password")
-  let emailValue=email.value;
+  let password = document.querySelector("#password");
+  let emailValue = email.value;
   let passwordValue = password.value;
 
   fetch("http://localhost:5678/api/users/login", {
@@ -24,24 +24,31 @@ document.querySelector("form").addEventListener("submit",function(event){
       .then(response => response.json()
       )
       .then(data => {
+        console.log(data)
 
-        /* Création du message d'erreur de connexion*/
-        let logError = document.createElement("p")
-        document.querySelector("h2").after(logError)
-        logError.style.color ="#E23D36"
-        document.querySelector("form").addEventListener("click", ()=>{
+        //Création d'un paragraphe d'erreur de connexion
+          let logError = document.createElement("p")
+
+        
+          document.querySelector("form").addEventListener("click", ()=>{
           logError.remove()
         })
 
-        /*Vérification de la valdité des valeurs de connexion*/
+        //Vérification de la valdité des valeurs de connexion
         if (data.token) {
           localStorage.setItem("token", data.token);
+          localStorage.setItem("userId", data.userId)
           window.location.replace("index.html")
 
         } else if (data.message) {
+          
+          document.querySelector("#email").after(logError)
+          logError.style.color ="#E23D36"
           logError.innerHTML = "Adresse Email inconnue"
 
         } else if (data.error) {
+          document.querySelector("#password").after(logError)
+          logError.style.color ="#E23D36"
           logError.innerHTML = "Veuillez vérifier votre mot de passe"
         } 
       })
