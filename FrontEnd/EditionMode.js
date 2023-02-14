82eb7a65973b7fe37245c3a8f33c9e36ab37ea21
Céline Gourc <celine.gourc1@laposte.net>
2023-02-14 18:18:37 +0100

    echange des querySelector par des noms de variables

--- FrontEnd/EditionMode.js
+++ FrontEnd/EditionMode.js
@@ -1,31 +1,18 @@
 if (userToken !== null){
 
-  document.querySelector("#loginButton").remove()
-  document.querySelector(".buttonContainer").remove()
-  document.querySelector("#logoutButton").addEventListener("click", ()=>{
+  const inputFile = document.querySelector("#file")
+  const uploadedImg = document.createElement("img")
+  uploadedImg.classList.add("uploadedImage")
+  const form = document.querySelector("#addWorkForm")
+  const parentSelect = document.querySelector("#categoryForm")
+  const emptyOption = document.createElement("option")
+
+  logInButton.remove()
+  workButtonContainer.remove()
+  logOutButton.addEventListener("click", ()=>{
     localStorage.removeItem("token")
     window.location.replace('index.html')
   })
-
-  fetch("http://localhost:5678/api/categories")  
-  .then(function(response) {
-    if (response.ok) {
-      return response.json();
-    }
-  })
-  .then(function(data){  
-    const parentSelect = document.querySelector("#categoryForm")
-    for(let category of data){
-      let optionCategory = document.createElement("option")
-      optionCategory.innerText = category.name
-      optionCategory.value = category.id
-      parentSelect.appendChild(optionCategory)
-    }
-  })
-  .catch (function(err) {
-    console.log('Une erreur est survenue',err)
-  })
-
 
   function displayWorkEdition(work){
     const figureEdition = document.createElement("figure")
@@ -42,6 +29,31 @@ if (userToken !== null){
     figureEdition.appendChild(figcaptionEdition)
   }
 
+function createOptionSelect(data){
+  parentSelect.appendChild(emptyOption)
+    for(let category of data){
+      let optionCategory = document.createElement("option")
+      optionCategory.innerText = category.name
+      optionCategory.value = category.id
+      parentSelect.appendChild(optionCategory)
+    }
+}
+
+  fetch("http://localhost:5678/api/categories")  
+  .then(function(response) {
+    if (response.ok) {
+      return response.json();
+    }
+  })
+  .then(function(data){  
+    createOptionSelect(data)
+  })
+  .catch (function(err) {
+    console.log('Une erreur est survenue',err)
+  })
+
+
+ 
   fetch("http://localhost:5678/api/works")
   .then(function(response) {
     if (response.ok) {
@@ -59,36 +71,40 @@ if (userToken !== null){
   })
             
   document.querySelector("#modalLink").addEventListener("click", ()=>{
-  document.querySelector("#modalWindow").style.display ="flex";
-  })
+    modalWindow.style.display ="flex";
+    })
                   
   document.querySelector("#openAddModal").addEventListener("click", ()=>{
-    document.querySelector("#addWork").style.display ="flex";
-    document.querySelector("#modalWindow").style.display="none";
+    addWorkWindow.style.display ="flex";
+    modalWindow.style.display="none";
   })
                 
   document.querySelector(".fa-xmark").addEventListener("click", ()=>{
-    document.querySelector("#modalWindow").style.display="none";
+    modalWindow.style.display="none";
   })
-  document.querySelector("#modalWindow").addEventListener("click", ()=>{
-    document.querySelector("#modalWindow").style.display="none";
+  modalWindow.addEventListener("click", ()=>{
+    modalWindow.style.display="none";
   })
                                  
   document.querySelector("#xClose").addEventListener("click", ()=>{
-    document.querySelector("#addWork").style.display="none";
-    document.querySelector("#addWorkForm").reset()
-    document.querySelector(".uploadedImage").remove()   
+    addWorkWindow.style.display="none";
+    form.reset()
+    if (document.querySelector(".uploadedImage")){
+      document.querySelector(".uploadedImage").remove()
+    }
   })
-  document.querySelector("#addWork").addEventListener("click", ()=>{
-    document.querySelector("#addWork").style.display="none";
-    document.querySelector("#addWorkForm").reset()
-    document.querySelector(".uploadedImage").remove()   
+  addWorkWindow.addEventListener("click", ()=>{
+    addWorkWindow.style.display="none";
+    form.reset()
+    if (document.querySelector(".uploadedImage")){
+      document.querySelector(".uploadedImage").remove()
+    } 
   })
   
   document.querySelector(".fa-arrow-left").addEventListener("click", ()=>{
-    document.querySelector("#addWork").style.display="none";
+    addWorkWindow.style.display="none";
     document.querySelector("#modalWindow").style.display="flex"
-    document.querySelector("#addWorkForm").reset()
+    form.reset()
     document.querySelector(".uploadedImage").remove() 
   })
                     
@@ -100,12 +116,11 @@ if (userToken !== null){
       })
     }
                 
-  const inputFile = document.querySelector("#file")  
+   
   inputFile.addEventListener('change', function () {
     let reader = new FileReader();
     reader.addEventListener("load", function(){
-      let uploadedImg = document.createElement("img")
-      uploadedImg.classList.add("uploadedImage")
+      
       uploadedImg.src= reader.result
       document.querySelector(".insertPhoto").append(uploadedImg);
     })
@@ -113,14 +128,14 @@ if (userToken !== null){
   });
 
 
-  const form = document.querySelector("#addWorkForm")
+  
   form.addEventListener("submit",(e)=>{
     e.preventDefault()
     console.log (inputFile.files[0].size)
     if(inputFile.files[0].size > 4000000){
       console.log("trop lourd")
     } else {
-      const formData = new FormData(addWorkForm)
+      const formData = new FormData(form)
       fetch("http://localhost:5678/api/works",{
         method: "POST",
         headers: {
@@ -133,8 +148,8 @@ if (userToken !== null){
       )
       .then(data => {
         console.log(data)
-        document.querySelector("#addWork").style.display="none"
-        document.querySelector("#addWorkForm").reset()
+        addWorkWindow.style.display="none"
+        form.reset()
         document.querySelector(".uploadedImage").remove()
         displayWork(data)
         displayWorkEdition(data)
