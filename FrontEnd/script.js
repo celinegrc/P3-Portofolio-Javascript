3da380393b4069fd7dccc4f902ad2ff57102d3ab
Céline Gourc <celine.gourc1@laposte.net>
2023-02-14 17:45:04 +0100

    ajout de fonctions

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,45 +1,34 @@
 let userToken = localStorage.getItem("token")
 
-
-
-document.querySelector("#modalWindow").style.display ="none";
-document.querySelector("#addWork").style.display ="none";
-document.querySelector("#deleteConfirm").style.display ="none";
-
-
-
 const workButtonContainer = document.createElement("ul")
-workButtonContainer.classList.add('buttonContainer')
-document.querySelector(".gallery").before(workButtonContainer)
-
-
-
- fetch("http://localhost:5678/api/categories")
-    
- .then(function(response) {
-   if (response.ok) {
-      return response.json();
-   }
- })
+const modalWindows = document.querySelector("#modalWindow")
+const addWorkWindow = document.querySelector("#addWork")
+const thumbnailWindow = document.querySelector("#deleteConfirm")
+const windowsEditionMode = [modalWindows, addWorkWindow, thumbnailWindow]
+const categoryButtons = document.querySelectorAll(".buttons")
+const logOutButton =  document.querySelector("#logoutButton")
+const headerEdition = document.querySelector(".editionMode")
+const EditionModeElement =[logOutButton, headerEdition, modalWindows, addWorkWindow, thumbnailWindow]
+const modifyTag = document.querySelectorAll(".modifTag")
  
- .then(function(categories){
-    const categorySet = new Set();
-    categorySet.add("Tous")
-    for (let category of categories){
-      categorySet.add(category.name)
-    }
-    for (let name of categorySet){
-      let workButton = document.createElement("li")
-      workButton.classList.add('buttons')
-      workButton.innerText = name
-      workButtonContainer.appendChild(workButton)
-    }
-  })
-  
- .catch (function(err) {
-   console.log('Une erreur est survenue',err)})
+function pushModifyTag() {
+  for (let tag of modifyTag){
+      EditionModeElement.push(tag)
+  }
+}
 
+function undisplayEditionModeElement(){
+  pushModifyTag()
+  for (let element of EditionModeElement){
+    element.remove()
+  }
+} 
 
+function undisplayWindowsEdition () {
+  for (let window of windowsEditionMode){
+    window.style.display ="none"
+  }
+}
 
 function displayWork(work){
   const workFigure = document.createElement("figure")
@@ -52,70 +41,104 @@ function displayWork(work){
   workFigure.appendChild(workFigcaption)
  }
 
+function createContainerFilterButtons(){
+workButtonContainer.classList.add("buttonContainer")
+document.querySelector(".gallery").before(workButtonContainer)
+}
 
+function createCategorySet(categories){
+  const categorySet = new Set();
+    categorySet.add("Tous")
+    for (let category of categories){
+    categorySet.add(category.name)
+  } createCategoryButtons(categorySet)
+}
+
+function createCategoryButtons(categorySet){
+  for (let name of categorySet){
+    let workButton = document.createElement("li")
+    workButton.classList.add('buttons')
+    workButton.innerText = name
+    workButtonContainer.appendChild(workButton)
+  } 
+}
+
+function styleActiveButtons(){
+   for (let button of categoryButtons){
+   if (button.innerText === "Tous"){
+     button.classList.add("active")
+   }
+   button.addEventListener("click",() => {
+     for (let activeButton of categoryButtons){
+       activeButton.classList.remove("active")   
+     }
+   button.classList.add("active")
+   })
+ }
+}
+
+
+function displayFilter(data){
+  const categoryButtons = document.querySelectorAll('.buttons')
+  for(let button of categoryButtons){
+    button.addEventListener("click",(e) => {
+      document.querySelector(".gallery").innerHTML=" ";
+      const workFiltres = data.filter(
+      data => data.category.name === e.target.innerText
+      )
+      for (let workFiltre of workFiltres){      
+        displayWork(workFiltre)
+      }
+      for (let work of data){       
+        if (button.innerText === "Tous") {
+          displayWork(work)
+        }
+      }
+    })
+  }   
+}
+
+
+ fetch("http://localhost:5678/api/categories") 
+ .then(function(response) {
+   if (response.ok) {
+      return response.json();
+   }
+  })
+ .then(function(categories){
+  createCategorySet(categories)
+  })
+ .catch (function(err) {
+   console.log('Une erreur est survenue',err)})
+
+   
 fetch("http://localhost:5678/api/works")
   .then(function(response) {
     if (response.ok) {
       return response.json();
     }
   })
-
   .then(function(data){
     for (let work of data){
       displayWork(work)
     }
     deleteWork(data)
-
-    const categoryButtons = document.querySelectorAll('.buttons')
-    for (let button of categoryButtons){
-      if (button.innerText === "Tous"){
-        button.classList.add("active")
-      }
-      button.addEventListener("click",() => {
-        for (let activeButton of categoryButtons){
-          activeButton.classList.remove("active")   
-        }
-      button.classList.add("active")
-      })
-    }
-
-
-    for(let button of categoryButtons){
-      button.addEventListener("click",(e) => {
-      document.querySelector(".gallery").innerHTML=" ";
-      const workFiltres = data.filter(
-        data => data.category.name === e.target.innerText
-        )
-      for (let workFiltre of workFiltres){      
-          displayWork(workFiltre)
-          }
-      for (let work of data){       
-          if (button.innerText === "Tous") {
-              displayWork(work)
-            }
-          }
-      })
-    }   
+    displayFilter(data) 
+    styleActiveButtons() 
   })
-    .catch (function(err) {
-    console.log('Une erreur est survenue',err)})
-
+  .catch (function(err) {
+    console.log('Une erreur est survenue',err)
+  })
 
 
 if (userToken === null){
-
-
-  let modifyTag = document.querySelectorAll(".modifTag")
-    for (let tag of modifyTag){
-      tag.remove()
-      }
-
-    document.querySelector("#logoutButton").remove()
-    document.querySelector(".editionMode").remove()
-    document.querySelector("#addWork").remove()
-    document.querySelector("#modalWindow").remove()
-    document.querySelector("#deleteConfirm").remove()
+  undisplayEditionModeElement()
 }
+
+createContainerFilterButtons()
+undisplayWindowsEdition ()
+
+
 
 
 
