a91d12c719a1b0a3f234dcba90cd6d4aec112644
Céline Gourc <celine.gourc1@laposte.net>
2023-02-20 11:18:42 +0100

    modification du nom du fichier

--- FrontEnd/editingMode.js
+++ FrontEnd/editingMode.js
@@ -3,11 +3,20 @@ const uploadedImg = document.createElement("img")
 const deleteImg = document.createElement("img")
 uploadedImg.classList.add("uploadedImage")
 const postForm = document.querySelector("#addWorkForm")
-const parentSelect = document.querySelector("#category")
 const emptyOption = document.createElement("option")
+const labelForCategory = document.createElement("label")
+labelForCategory.innerText="Catégorie"
+labelForCategory.setAttribute("for","category")
+const selectCategory = document.createElement("select")
+selectCategory.setAttribute("name","category"  )
+selectCategory.setAttribute("id","category")
+selectCategory.setAttribute("required","required")
 const galleryEdition = document.querySelector(".galleryEdition")
 const jsStop = document.querySelectorAll(".jsStop")
 const postBtn = document.querySelector("#validButton")
+const arrow = document.querySelector(".fa-arrow-left")
+const xClose = (document.querySelector("#xClose"))
+const tooLoudMessage = document.createElement('p')
 
 function displayWorkEdition(work){
   const figureEdition = document.createElement("figure")
@@ -25,12 +34,14 @@ function displayWorkEdition(work){
 }
 
 function createOptionSelect(data){
-  parentSelect.appendChild(emptyOption)
+  document.querySelector(".forBorder").append(labelForCategory)
+  labelForCategory.after(selectCategory)
+  selectCategory.appendChild(emptyOption)
     for(let category of data){
       let optionCategory = document.createElement("option")
       optionCategory.innerText = category.name
       optionCategory.value = category.id
-      parentSelect.appendChild(optionCategory)
+      selectCategory.appendChild(optionCategory)
     }
 }
 
@@ -71,7 +82,7 @@ function createDeleteThumbnail(data){
   deleteImg.classList.add("deleteImg") 
 }
 
-function closeDeleteThumbnailEvent(){
+function closeDeleteThumbnail(){
   deleteImg.remove()
   thumbnailWindow.style.display="none" 
 }
@@ -87,27 +98,30 @@ function reader(){
       reader.readAsDataURL(inputFile.files[0])
     } else {
       fileTooLoud()
+      resetFileTooLoud(xClose)
+      resetFileTooLoud(arrow)
+      resetFileTooLoud(inputFile)
+      resetFileTooLoud(addWorkWindow)
     }   
   })
 }
 
 function fileTooLoud(){
-  const fileTooLoud = document.createElement('p')
-  document.querySelector(".insertPhoto").append(fileTooLoud)
-  fileTooLoud.innerText="Le fichier est trop volumineux"
-  fileTooLoud.style.color = "#E23D36"
-  fileTooLoud.style.fontWeight="bold"
-  document.querySelector(".addContent").addEventListener("click",()=>{
-      fileTooLoud.innerText=" "
-  })
+  document.querySelector(".insertPhoto").before(tooLoudMessage)
+  tooLoudMessage.innerText="Le fichier est trop volumineux"
+  tooLoudMessage.style.color = "#E23D36"
+  tooLoudMessage.style.margin="0px"
   document.querySelector("#fileSizeMax").style.color = "#E23D36"
-  document.querySelector("#fileSizeMax").style.fontWeight="bold"
-  document.querySelector(".addContent").addEventListener("click",()=>{
-  document.querySelector("#fileSizeMax").style.color = "black"
-  document.querySelector("#fileSizeMax").style.fontWeight="normal"
-})
 }
 
+function resetFileTooLoud(element){
+  element.addEventListener("click",()=>{
+    tooLoudMessage.innerText=" "
+    document.querySelector("#fileSizeMax").style.color = "black"
+    reset()
+  })
+}
+  
 function jsStopPropagation(){
   for (let element of jsStop) {
     element.addEventListener("click", (e)=>{
@@ -116,39 +130,49 @@ function jsStopPropagation(){
   }
 } 
 
-function fetchDisplayEdition(){
-  fetch("http://localhost:5678/api/works")
-  .then(function(response){
-    if (response.ok) {
-      return response.json();
-    }
-  })
-  .then(function(data){
-    galleryEdition.innerHTML=" "
-    for (let work of data){
-      displayWorkEdition(work)
-    }
-  deleteWork(data)
-  })
-  .catch (function(err) {
-  console.log('Une erreur est survenue',err)
+function logOut(){
+  logOutButton.addEventListener("click", ()=>{
+    localStorage.removeItem("token")
+    window.location.replace("index.html")
+    //fetchDisplayWorksHome()
   })
 }
 
-function fetchCreateOption(){
-  fetch("http://localhost:5678/api/categories")  
-  .then(function(response){
-    if (response.ok) {
-      return response.json();
-    }
-  })
-  .then(function(data){  
-    createOptionSelect(data)
-  })
-  .catch (function(err) {
-    console.log('Une erreur est survenue',err)
+function eventCloseThumbnail(){
+  document.querySelector("#closeBtn").addEventListener("click",()=>{
+  closeDeleteThumbnail()
+  fetchDisplayEdition()
+  fetchDisplayWorksHome()
   })
 }
+
+async function fetchDisplayEdition(){
+  try {
+    const response = await  fetch("http://localhost:5678/api/works")
+    const data = await response.json()
+    galleryEdition.innerHTML=" "
+      for (let work of data){
+        displayWorkEdition(work)
+      }
+    deleteWork(data)
+  }
+  catch (err) {
+  console.log('Une erreur est survenue',err)
+  }
+}
+
+async function fetchCreateOption(){
+  try {
+  const response = await fetch("http://localhost:5678/api/categories")
+  const data = await response.json()
+  createOptionSelect(data)
+  }
+  catch (err) {
+    console.log('Une erreur est survenue',err)
+  }
+}
+
+
 
 function deleteWork(data){
   const trashes = document.querySelectorAll(".fa-trash-can")
@@ -163,54 +187,46 @@ function deleteWork(data){
         }
       })
       .then (response => {
-        if(response.status === 204 ) {
+        if (response.status === 204 ) {
           modalWindow.style.display="none";
           thumbnailWindow.style.display="flex";
           createDeleteThumbnail(data[index])
-          document.querySelector("#closeBtn").addEventListener("click",()=>{
-            closeDeleteThumbnailEvent()
-            fetchDisplayEdition()
-            fetchDisplayWorksHome() 
-          })    
+          eventCloseThumbnail()     
         } 
       })            
     }) 
   } 
 }
 
-function postWork(){ 
-  postForm.addEventListener("submit",(e)=>{
-   e.preventDefault()
-    if(inputFile.files[0].size < 400000){
-      const formData = new FormData(postForm)
-      fetch("http://localhost:5678/api/works",{
-        method: "POST",
-        headers: {
-          'authorization': `Bearer ${userToken}`,
-          'accept':'application/json',
-        },
-        body: formData   
-      })
-      .then(response => {
-        console.log(response)
-        reset()
-        addWorkWindow.style.display="none"
-        fetchDisplayEdition()
-        fetchDisplayWorksHome() 
-      })
-      .catch (function(err) {
-        console.log('Une ERREUR est survenue',err)
-      })
-    }
-  }) 
+async function fetchPostWork(formData){
+  try{
+    const response = await fetch("http://localhost:5678/api/works",{
+      method: "POST",
+      headers: {
+        'authorization': `Bearer ${userToken}`,
+        'accept':'application/json',
+      },
+      body: formData   
+    })
+    const rep = await response.json() 
+      reset()
+      addWorkWindow.style.display="none"
+      fetchDisplayEdition()
+      fetchDisplayWorksHome()    
+  }
+  catch (err) {
+    console.log('Une ERREUR est survenue',err)
+  }
 }
 
-function logOut(){
-  logOutButton.addEventListener("click", ()=>{
-    localStorage.removeItem("token")
-    window.location.replace("index.html")
-   fetchDisplayWorksHome()
-  })
+function postWork(){ 
+  postForm.addEventListener("submit",(e)=>{
+    e.preventDefault()
+    if(inputFile.files[0].size < 400000){
+      const formData = new FormData(postForm)
+      fetchPostWork(formData)
+    }
+  }) 
 }
 
 if (userToken !== null){
@@ -223,28 +239,24 @@ if (userToken !== null){
   closeModalWindow(document.querySelector(".fa-xmark"))
   closeModalWindow(modalWindow)
   closeModalWindow(document.querySelector("#openAddModal"))
-  closeAddWorkWindow(document.querySelector("#xClose"))
-  closeAddWorkWindow(document.querySelector(".fa-arrow-left"))
-  //closeAddWorkWindow(document.querySelector("#validButton"))
+  closeAddWorkWindow(xClose)
+  closeAddWorkWindow(arrow)
   closeAddWorkWindow(addWorkWindow)
   jsStopPropagation()
   postWork()
   formReset(addWorkWindow)
-  formReset(document.querySelector("#xClose"))
-  formReset(document.querySelector(".fa-arrow-left"))
+  formReset(xClose)
+  formReset(arrow)
   
   document.querySelector("#modalLink").addEventListener("click", ()=>{
     modalWindow.style.display ="flex";
-  })
-                  
+  })             
   document.querySelector("#openAddModal").addEventListener("click", ()=>{
     addWorkWindow.style.display ="flex";
   })
-  
-  document.querySelector((".fa-arrow-left")).addEventListener("click", ()=>{
+  arrow.addEventListener("click", ()=>{
     modalWindow.style.display ="flex";
-  })
-  
+  }) 
 }
 
 
